52c0d1532a3f73bcb0a445043aa836d5f8f6a2f3
abe <abe@nagisa-inc.jp>
2020-01-03 15:35:32 +0900

    first commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,6 @@
 
 SERVICE_NAME=$1
 
+cd ./services/$SERVICE_NAME
 docker build -t howto-grpc/$SERVICE_NAME .
 docker tag howto-grpc/$SERVICE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/howto-grpc/$SERVICE_NAME:$CIRCLE_SHA1
